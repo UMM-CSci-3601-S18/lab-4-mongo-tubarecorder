d21c16c9d602452da15d9bddb17901eae3cc99f2
hamer073 <hamer073@morris.umn.edu>
2018-02-20 15:40:30 -0600

    Add getTodos -Currently just returns all the todos

--- server/src/main/java/umm3601/todo/TodoController.java
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -1,4 +1,36 @@
 package umm3601.todo;
 
+import com.google.gson.Gson;
+import com.mongodb.*;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.util.JSON;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import java.util.Iterator;
+import java.util.Map;
+
+import static com.mongodb.client.model.Filters.eq;
+
 public class TodoController {
+
+    private final Gson gson;
+    private MongoDatabase database;
+    private final MongoCollection<Document> todoCollection;
+
+    public TodoController(MongoDatabase database) {
+        gson = new Gson();
+        this.database = database;
+        todoCollection = database.getCollection("todos");
+    }
+
+    public String getTodos(Map<String, String[]> queryParams) {
+        Document filterDoc = new Document();
+
+
+        FindIterable<Document> mathchingTodos = todoCollection.find(filterDoc);
+
+        return JSON.serialize(mathchingTodos);
+    }
 }

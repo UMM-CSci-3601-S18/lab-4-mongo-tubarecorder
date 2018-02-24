a6a7dc60a42ac65bd7ef28824a4e06b9784127de
hamer073 <hamer073@morris.umn.edu>
2018-02-24 14:21:07 -0600

    Add a test for checking that the page loads properly Add a test for filtering by owner, will work with proper client side owner filtering

---
+++ client/e2e/todo-list.e2e-spec.ts
@@ -0,0 +1,26 @@
+import {TodoPage} from './todo-list.po';
+import {browser, protractor, element, by} from 'protractor';
+import {Key} from 'selenium-webdriver';
+
+const origFn = browser.driver.controlFlow().execute;
+
+describe('Todo List', () => {
+    let page: TodoPage;
+
+    beforeEach(()=>{
+        page = new TodoPage;
+    });
+
+    it('Should get and highlight the Todos title attribute ', () =>{
+        page.navigateTo();
+        expect(page.getTodoTitle()).toEqual('Todos');
+    })
+
+    /*
+    it('Should type in the Search by Owner field and filter the todos', () =>{
+        page.navigateTo();
+        page.typeAnOwner('Barry');
+        expect(element.all(by.className('todos')).first().getText()).toContain('Barry');
+    })
+    */
+});
---
+++ client/e2e/todo-list.po.ts
@@ -0,0 +1,34 @@
+import {browser, element, by, promise, ElementFinder} from 'protractor';
+import {Key} from 'selenium-webdriver';
+
+export class TodoPage{
+    navigateTo(): promise.Promise<any> {
+        return browser.get('/todos');
+    }
+
+    highlightElement(byObject) {
+        function setStyle(element, style) {
+            const previous = element.getAttribute('style');
+            element.setAttribute('style', style);
+            setTimeout(() => {
+                element.setAttribute('style', previous);
+            }, 200);
+            return 'highlighted';
+        }
+        return browser.executeScript(setStyle, element(byObject).getWebElement(), 'color: red; background-color: yellow;');
+    }
+
+    getTodoTitle() {
+        const title = element(by.id('todo-list-title')).getText();
+        this.highlightElement(by.id('todo-list-title'));
+
+        return title;
+    }
+
+    typeAnOwner(owner: string) {
+        const input = element(by.id('todoOwner'));
+        input.click();
+        input.sendKeys('owner');
+    }
+
+}
